26cd82adbd941adfe7ab243d5ef196a4d5af7766
lazar030 <lazarjankovic030@gmail.com>
2019-10-02 16:32:02 +0200

    Updates

---
+++ precache-manifest.79e879c47d9ee8502a725a0238ef3c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "891972cf984213e29867",
+    "url": "/portfolio/static/css/main.5e869661.chunk.css"
+  },
+  {
+    "revision": "891972cf984213e29867",
+    "url": "/portfolio/static/js/main.891972cf.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "00c173e4f45613b070b3",
+    "url": "/portfolio/static/js/2.00c173e4.chunk.js"
+  },
+  {
+    "revision": "d9dd941b7528921e3b2ee9a9803d83c0",
+    "url": "/portfolio/index.html"
+  }
+];
